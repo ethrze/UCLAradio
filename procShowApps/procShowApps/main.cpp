7764fc824010f34f517e45a3701a13c82e616744
ethrze <ethrze@gmail.com>
2015-05-29 23:43:20 -0700

    beginning C++ code

---
+++ procShowApps/procShowApps/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  procShowApps
+//
+//  Created by Grace Phillips on 5/29/15.
+//  Copyright (c) 2015 UCLA. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
